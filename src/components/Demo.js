683293b226fad85d7ff61ebb73bc19917e5f58c0
Jonatan Pinto <jonatan.pinto075@gmail.com>
2022-07-22 14:22:01 -0500

    main

---
+++ src/components/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from "./Demo.module.css"
+
+function Demo() {
+  return (
+    <div className={classes.inner}>
+        Demo
+    </div>
+  )
+}
+
+export default Demo
